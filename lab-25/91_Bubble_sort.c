b2240cc3ac6a1024f11cb9c3c64d3c474bae05a1
RuchitaSureliya09 <ruchitasureliya1609@gmail.com>
2026-09-21 18:09:23 +0530

    edit file

--- lab-25/91_Bubble_sort.c
+++ lab-25/91_Bubble_sort.c
@@ -4,20 +4,20 @@
 
 void main()
 {
-    int a[100], n, i, j, temp;
+    int a[100], n, temp;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
     printf("Enter elements:\n");
-    for(i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
 
-    for(i = 0; i < n - 1; i++)
+    for(int i = 0; i < n - 1; i++)
     {
-        for(j = 0; j < n - i - 1; j++)
+        for(int j = 0; j < n - i - 1; j++)
         {
             if(a[j] > a[j + 1])
             {
@@ -30,7 +30,7 @@ void main()
 
     printf("Bubble Sorted array:\n");
 
-    for(i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
     }
