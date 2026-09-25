e91c60cc0bb4f705c8d65c0c4efe6a0b34c32f7e
RuchitaSureliya09 <ruchitasureliya1609@gmail.com>
2026-09-25 10:07:34 +0530

    add file

---
+++ lab-26/93_Insertion_sort.c
@@ -0,0 +1,38 @@
+// 93. Write a program to implement Insertion Sort using Array.
+
+#include <stdio.h>
+
+void main()
+{
+    int a[100], n, i, j, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 1; i < n; i++)
+    {
+        key = a[i];
+        j = i - 1;
+
+        while(j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+
+    printf("Sorted array by Insertion Sort :\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
