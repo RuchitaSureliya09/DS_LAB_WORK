b6524d05a9a3a5479657e99a4a0bf18b0e43fd33
RuchitaSureliya09 <ruchitasureliya1609@gmail.com>
2026-09-26 19:19:47 +0530

    edit file

--- lab-26/93_Insertion_sort.c
+++ lab-26/93_Insertion_sort.c
@@ -4,21 +4,21 @@
 
 void main()
 {
-    int a[100], n, i, j, key;
+    int a[100], n, key;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
     printf("Enter elements:\n");
-    for(i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
 
-    for(i = 1; i < n; i++)
+    for(int i = 1; i < n; i++)
     {
         key = a[i];
-        j = i - 1;
+        int j = i - 1;
 
         while(j >= 0 && a[j] > key)
         {
@@ -31,7 +31,7 @@ void main()
 
     printf("Sorted array by Insertion Sort :\n");
 
-    for(i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
     }
